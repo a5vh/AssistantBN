94430edb56eff3f852088006dec03bb90d996649
August H. Hummert V <august.hummert5@gmail.com>
2019-03-13 12:45:33 -0500

    3/13
    help pls this is terrible

--- app/src/main/java/com/a5vh/assistant/GoalsFragment.java
+++ app/src/main/java/com/a5vh/assistant/GoalsFragment.java
@@ -1,12 +1,15 @@
 package com.a5vh.assistant;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 public class GoalsFragment extends Fragment {
 
